5a39387f6bf69197ea410b84ccee6ecfe990548a
tharakajrsv <tharakajrsv@gail.com>
2025-04-12 10:26:14 +0530

    adding git acctions

--- cucumber.js
+++ cucumber.js
@@ -1,10 +1,11 @@
+// cucumber.js
 module.exports = {
-    default: `
-    --require ./support/hooks.js 
-    --require ./support/world.js 
+  default: `
+    --require ./support/hooks.js
+    --require ./support/world.js
     --require ./step_definitions/**/*.js
     --format pretty
     --format "allure-cucumberjs/reporter"
-    --format-options '{"resultsDir": "./allure-results"}'`
-  };
-  
+    --format-options '{"resultsDir": "./allure-results"}'
+  `,
+};
--- support/hooks.js
+++ support/hooks.js
@@ -1,11 +1,12 @@
-const { Before, After, setDefaultTimeout } = require('@cucumber/cucumber');
-const { chromium } = require('playwright');
-const orgPageObjects = require('../pages/orgPageObjects'); // Import the centralized setup
-require('dotenv').config();
+const { Before, After, setDefaultTimeout } = require("@cucumber/cucumber");
+const { chromium } = require("playwright");
+const orgPageObjects = require("../pages/orgPageObjects"); // Import the centralized setup
+const allure = require("allure-cucumberjs");
+require("dotenv").config();
 let browser, context, page;
 
 // Set default timeout
-setDefaultTimeout(60 * 1000);  // 60 seconds timeout
+setDefaultTimeout(60 * 1000); // 60 seconds timeout
 
 // Hook before each scenario
 Before(async function () {
@@ -17,6 +18,14 @@ Before(async function () {
   // Store page in the "this" object to be used in step definitions
   this.page = page;
   this.orgPageObjects = orgPageObjects.createPages(this.page);
+});
+
+// Hook after each scenario step
+After(async function ({ result }) {
+  if (result.status !== "passed") {
+    const screenshot = await this.page.screenshot();
+    await allure.createAttachment("Screenshot", screenshot, "image/png");
+  }
 });
 
 // Hook after each scenario
